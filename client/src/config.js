eebc4fee0fd8712fa043ba0bc2619c5dbd708440
Shreyash Kadam <shreyash.kadam10@gmail.com>
2023-04-26 12:16:23 +0530

    Config update

--- client/src/config.js
+++ client/src/config.js
@@ -6,4 +6,3 @@ export const API_SERVICE = 'http://localhost:8080/api/v1/main';
 export const API_SERVICE_1 = 'https://aimassist-server.onrender.com/api/v1';
 
 export const PUBLIC_URL = 'http://localhost:3000'
-// export const PUBLIC_URL = 'https://dogcaresaas.onrender.com'
